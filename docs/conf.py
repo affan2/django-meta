7dec8a33a508611e2b8550aab7d71974f7580642
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-07-29 12:38:22 +0400

    Remove redundant unicode strings

--- docs/conf.py
+++ docs/conf.py
@@ -66,10 +66,10 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Django Meta'
-copyright = u'2014, Iacopo Spalletti'
+project = 'Django Meta'
+copyright = '2014, Iacopo Spalletti'
 
-# The version info for the project you're documenting, acts as replacement for
+# The version info for the project yo're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
@@ -223,8 +223,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'djangoCMSHelper.tex', u'Django App Helper Documentation',
-   u'Iacopo Spalletti', 'manual'),
+  ('index', 'djangoCMSHelper.tex', 'Django App Helper Documentation',
+   'Iacopo Spalletti', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -253,8 +253,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'djangometa', u'Django Meta Documentation',
-     [u'Iacopo Spalletti'], 1)
+    ('index', 'djangometa', 'Django Meta Documentation',
+     ['Iacopo Spalletti'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -267,8 +267,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'djangometa', u'Django Meta Documentation',
-   u'Iacopo Spalletti', 'djangometa', 'One line description of project.',
+  ('index', 'djangometa', 'Django Meta Documentation',
+   'Iacopo Spalletti', 'djangometa', 'One line description of project.',
    'Miscellaneous'),
 ]
 
